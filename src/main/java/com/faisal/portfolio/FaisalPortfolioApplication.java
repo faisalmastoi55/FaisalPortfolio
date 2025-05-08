c0155897fb8d00c5cce3bafde65eaafa07e764e3
Faisal Ali <faisalmastoi341@gmail.com>
2025-05-08 15:01:14 +0500

    Initial commit

---
+++ src/main/java/com/faisal/portfolio/FaisalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.faisal.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaisalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaisalPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/faisal/portfolio/controller/MyController.java
@@ -0,0 +1,52 @@
+package com.faisal.portfolio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.faisal.portfolio.helper.Message;
+import com.faisal.portfolio.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class MyController {
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping()
+	public String portfolio()
+	{
+		
+		return "faisal_portfolio";
+	}
+	
+	@PostMapping("/sendMail")
+	public String sendMail(@RequestParam("name") String name, @RequestParam("email") String email,
+			@RequestParam("subject") String subject1, @RequestParam("message") String message1,
+			HttpSession session)
+	{
+		String subject = "Client Message";
+		String message = "" + "<div>" + "<h2>" + "Name : "+"<b>" + name +"</b>" + "</h2>" 
+								+ "<h2>" + "Email : "+"<b>" + email +"</b>" + "</h2>"  
+								+ "<h2>" + "Subject : "+"<b>" + subject1 +"</b>" + "</h2>" 
+								+ "<h4>" + "Message : "+"<i>" + message1 +"</i>" + "</h4>" + "</div>";
+		String to = "faisalmastoi341@gmail.com";
+
+		boolean sendEmail = this.userService.sendEmail(subject, message, to);
+
+		if (sendEmail) {
+			
+			session.setAttribute("message", new Message("Your message has been sent.", "alert-success"));
+				
+		} else {
+
+			session.setAttribute("message", new Message("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!", "alert-danger"));
+			
+		}
+		return "redirect:/#contact";
+	}
+}
